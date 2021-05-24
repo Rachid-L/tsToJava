b6f70cbb0b00137b5a3d55836a2afcf9efc4bd77
Rachid-L <rachid-leboukh_student2021@wilder.school>
2021-05-24 10:11:39 +0200

    exercice pushed

---
+++ User.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+import java.util.ArrayList;
+
+public class User {
+    private Date birthdate;
+    private int size;
+    private ArrayList<Photo> photos;
+    private Address address;
+}
